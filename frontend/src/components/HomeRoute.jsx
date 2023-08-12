9d9922c6ca0da1ce8bceaa567708c64a1a3ce8d9
Filipe <moscovo.filipe@gmail.com>
2023-08-12 16:29:40 -0600

    Fetching the Photo data

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -1,21 +1,49 @@
 // frontend/src/components/HomeRoute.jsx
 
-import React from "react";
+import React, { useEffect, useReducer } from "react";
 import TopNavigationBar from "./TopNavigationBar";
 import PhotoList from "./PhotoList";
-import photos from "../mocks/photos";
-import topics from "../mocks/topics";
 import "../styles/HomeRoute.scss";
 
-/**
- * HomeRoute Component
- * Displays the top navigation bar and a list of photos.
- */
-const HomeRoute = () => (
-  <div className="home-route">
-    <TopNavigationBar topics={topics} />
-    <PhotoList photos={photos} />
-  </div>
-);
+const ACTIONS = {
+  SET_PHOTO_DATA: "SET_PHOTO_DATA",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.SET_PHOTO_DATA:
+      return { ...state, photos: action.payload.photos };
+    default:
+      return state;
+  }
+}
+
+const HomeRoute = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    photos: [],
+    topics: [],
+  });
+
+  useEffect(() => {
+    fetch("http://localhost:8001/api/photos")
+      .then((response) => response.json())
+      .then((data) => {
+        dispatch({
+          type: ACTIONS.SET_PHOTO_DATA,
+          payload: { photos: data }, // Use the whole data as photos
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching photo data:", error);
+      });
+  }, []);
+
+  return (
+    <div className="home-route">
+      <TopNavigationBar topics={state.topics} />
+      <PhotoList photos={state.photos} />
+    </div>
+  );
+};
 
 export default HomeRoute;
